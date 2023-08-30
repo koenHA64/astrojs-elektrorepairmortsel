163d6531e198e0b637df28c637fae3a0c3e91175
Jelle VdP <jellevandepoel@gmail.com>
2023-08-30 14:48:28 +0200

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jelle-vdp.github.io/",
+  base: "/astrojs-elektrorepairmortsel"
   integrations: [tailwind()]
 });
